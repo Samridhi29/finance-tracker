ebc6a14b607468f8cafa2c05969a1bca53697103
samridhi29 <jhasamridhi29@gmail.com>
2025-07-01 09:18:40 +0530

    first commit

---
+++ hooks/use-fetch.js
@@ -0,0 +1,29 @@
+//to handle api calls when we submit te form, store data(eg- create acc drawer form in dashboard)
+import { useState } from "react";
+import { toast } from "sonner";
+//for api call
+const useFetch = (cb) => {
+  const [data, setData] = useState(undefined);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fn = async (...args) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await cb(...args);  //cb-callback
+      setData(response);
+      setError(null);
+    } catch (error) {
+      setError(error);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { data, loading, error, fn, setData };
+};
+
+export default useFetch;
